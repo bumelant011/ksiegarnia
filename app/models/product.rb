b5821cda315c6f62715350dcf0add4af23d1ace4
Slawek <sawko@poczta.fm>
2012-11-20 15:06:14 +0100

    testy dzien 2

---
+++ app/models/product.rb
@@ -0,0 +1,18 @@
+class Product < ActiveRecord::Base
+  attr_accessible :description, :image_url, :price, :title
+	
+  validates :title, :image_url, :description,
+  :presence => true
+
+  validates :title, :uniqueness => true
+
+  validates :image_url, :allow_blank => true,
+    format: {
+      with: %r{\.(gif|jpg|png|)\Z}i,
+     message: 'must be a url for gif, jpg, png image.'
+    }
+
+  validates :price, numericality: {
+    greater_than_or_equal_to: 0.1
+    }
+end
---
+++ test/unit/product_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'atrybuty produktu nie moga byc puste' do
+    product = Product.new 
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'cena musi byc wieksza od zera'  do
+    product = Product.new title: "Moja ksiazka", description: "Lorem ipsum dodle", image_url: "tet.jpg"
+
+    product.price = -1
+    assert product.invalid?
+
+    product.price = 0
+    assert product.invalid?
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "Moja ksiazka", description: "opis", price: 1,
+    image_url: image_url)
+  end
+  
+  test 'url moga prowadzic tylko do obrazkow' do
+    ok = %w{ obr.gif obr.jpg obr.png obr.JPG obr.Jpg
+        http://a.b.c/x/y/z/obr.gif}
+    bad = %w{ obr.doc obr.gif/more obr.gif.more}
+
+  ok.each do |name|
+  assert new_product(name).valid?
+  end
+
+  bad.each do |name|
+  assert new_product(name).invalid?
+  end
+end
+
+end
