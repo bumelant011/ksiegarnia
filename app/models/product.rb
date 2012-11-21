315747caa0930192f954227c1af6af07b86d0bbc
Slawek <sawko@poczta.fm>
2012-11-21 15:24:51 +0100

    dzien 3 - ajax itp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,16 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 	
-  validates :title, :image_url, :description,
-  :presence => true
+  validates :title, :image_url, :description, :presence => true
 
   validates :title, :uniqueness => true
 
-  validates :image_url, :allow_blank => true,
-    format: {
-      with: %r{\.(gif|jpg|png|)\Z}i,
-     message: 'must be a url for gif, jpg, png image.'
-    }
+  validates :image_url,
+      allow_blank: true,
+      format: {
+        with:    %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+      }
 
   validates :price, numericality: {
     greater_than_or_equal_to: 0.1
